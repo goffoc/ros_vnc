4523154ff7722c8a095fd77aab453313d7cfddd8
Chris Gofton <chris.gofton@gmail.com>
2024-11-12 17:32:26 +1000

    new changes

--- runbuild.sh
+++ runbuild.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 #Build Docker Image
-cd /home/chris/dockerfiles/vnc_jazzy && sudo docker buildx build --platform=linux/amd64 --progress=plain -t myros2 .
+cd /home/chris/dockerfiles/vnc_jazzy && docker buildx build --platform=linux/amd64 --progress=plain -t myros2 .
