27ff9fb9eead12a9c64fb4592612e69452661719
Chris Gofton <chris.gofton@gmail.com>
2024-11-24 12:18:48 +1000

    added source bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -361,4 +361,5 @@ echo "==========================================================================
 #exec /bin/tini -- supervisord -n -c /etc/supervisor/supervisord.conf
 /usr/bin/vncserver :1 -geometry 1920x1080 -depth 24
 websockify --web=/usr/lib/novnc 80 localhost:5901
+echo "source /opt/ros/jazzy/setup.bash" >> /root/.bashrc
 /bin/bash
