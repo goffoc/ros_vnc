caa920dcf556842f0ee7d9d8111659d1a67ea1a3
Chris <chris.gofton@gmail.com>
2024-11-24 22:44:08 +1100

    added ros port

--- runcont.sh
+++ runcont.sh
@@ -1,2 +1,2 @@
 #docker run -p 6080:80 --security-opt seccomp=unconfined --shm-size=512m myros2
-docker run -it -p 6080:80 --security-opt seccomp=unconfined --shm-size=512m myros2
+docker run -it -p 6080:80 -p 11311:11311 --security-opt seccomp=unconfined --shm-size=512m myros2
